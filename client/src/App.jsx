cd0d54503bf27b0df148bec7c9dbae0597fcd028
Bamo <70605142+bamo100@users.noreply.github.com>
2024-07-03 17:02:42 +0100

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import Routes from "./route";
 
 function App() {
   const apiUrl = process.env.API_BASE_URL;
-  // const isProduction = process.env === 'production';
+  const isProduction = process.env === 'production';
   axios.defaults.baseURL = isProduction ? apiUrl : 'http://localhost:4040';
   axios.defaults.withCredentials = true;
 
